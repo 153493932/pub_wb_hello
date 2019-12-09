aae2de1fd1b3abca9b931c54962af385dd85cd7e
wanghaijun01 <wanghaijun01@58.com>
2019-12-09 18:59:52 +0800

    dart init

---
+++ bin/main.dart
@@ -0,0 +1,11 @@
+import 'src/Developer.dart';
+
+// 顶层函数
+sayHello() {
+  new Developer().say();
+}
+
+// 执行入口
+void main() {
+  sayHello();
+}
---
+++ bin/src/Developer.dart
@@ -0,0 +1,8 @@
+// 类
+class Developer {
+  // 实例方法
+  say() {
+    // print是全局打印函数，类似于JavaScript的console.log和Java的System.out.print
+    print('Hello World232323');
+  }
+}
